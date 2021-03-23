bd524706da449faa802b02dfbda80ad425a75c5f
Yechani-0422 <63704943+Yechani-0422@users.noreply.github.com>
2021-03-23 21:54:24 +0900

    JumpGroundCheckFixed

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -16,11 +16,13 @@ public class PlayerMove : MonoBehaviour
 
     Rigidbody2D rigidbody;
     SpriteRenderer spriteRenderer;
+    BoxCollider2D boxCollider2d;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -28,8 +30,7 @@ public class PlayerMove : MonoBehaviour
     {
         Move();
         Jump();
-        SpriteFlip();       
-       
+        SpriteFlip();
     }
 
     //이동
@@ -50,14 +51,11 @@ public class PlayerMove : MonoBehaviour
     //점프
     void Jump()
     {
-      
-       if (Input.GetButtonDown("Jump"))
+      if (isJump==true&&Input.GetKeyDown(KeyCode.Space))
        {
-             if(isJump==true)
-             {
-                rigidbody.AddForce(new Vector2(0, maxJump), ForceMode2D.Impulse);
-                isJump = false;
-             }
+            
+             rigidbody.AddForce(new Vector2(0, maxJump), ForceMode2D.Impulse);
+             isJump = false;
         }
     }
 
@@ -76,13 +74,47 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    //점프가능여부
+    ////점프가능여부
+    //private bool IsGrounded()
+    //{
+    //    float extraHeightText = 0.1f;
+    //    RaycastHit2D rayhit = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0, Vector2.down, extraHeightText, LayerMask.GetMask("Ground"));
+    //    Color rayColor;
+
+    //    if(rayhit.collider!=null)
+    //    {
+    //        rayColor = Color.green;
+    //    }
+    //    else
+    //    {
+    //        rayColor = Color.red;
+    //    }
+
+    //    Debug.DrawRay(boxCollider2d.bounds.center + new Vector3(boxCollider2d.bounds.extents.x, 0), Vector2.down * (boxCollider2d.bounds.extents.y + extraHeightText), rayColor);
+    //    Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, 0), Vector2.down * (boxCollider2d.bounds.extents.y + extraHeightText), rayColor);
+    //    Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, boxCollider2d.bounds.extents.y + extraHeightText), Vector2.right *( boxCollider2d.bounds.extents.x * 2f), rayColor);
+
+    //    return rayhit.collider != null;
+    //}
+
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.tag=="FLOOR")
+        if (other.gameObject.tag == "FLOOR")
         {
-            isJump = true;
-            Debug.Log("true");
+            if(rigidbody.velocity.y<=0)
+            {
+                isJump = true;
+                Debug.Log("JumpTrue");
+            }           
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "FLOOR")
+        {           
+             isJump = false;
+             Debug.Log("JumpFalse");
         }
     }
 }
