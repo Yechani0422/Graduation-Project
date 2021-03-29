0d709e3122e54cdabda4b8988e844112f5dfb26e
wed2425 <54612425+wed2425@users.noreply.github.com>
2021-03-29 23:51:01 +0900

    MainMenu
    간단한 시작 화면 제작

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnclickNEW()
+    {
+        Debug.Log("NEW");
+        SceneManager.LoadScene("TestScene");
+    }
+    public void OnclickLOAD()
+    {
+        Debug.Log("LOAD");
+    }
+    public void OnclickQUIT()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        Debug.Log("QUIT");
+#else
+            Application.Quit();
+#endif
+
+    }
+}
