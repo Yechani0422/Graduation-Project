4c824b628999585f5634f808568e16c2a9c2af3f
wed2425 <54612425+wed2425@users.noreply.github.com>
2021-04-06 02:07:08 +0900

    Cursor(ver.1)
    커서 디폴트와 클릭 시 이미지 변경
    다음에 상자 부분 사진촬영예정

---
+++ Assets/Script/CursorManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+
+    public Texture2D cursor;
+    public Texture2D leftCursorClicked;
+    public Vector2 MouseHotSpot = Vector2.zero;
+    [SerializeField]
+    public Vector2 leftClickedHotSpot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursor, MouseHotSpot, CursorMode.Auto);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Cursor.SetCursor(leftCursorClicked, leftClickedHotSpot, CursorMode.ForceSoftware);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            Cursor.SetCursor(cursor, MouseHotSpot, CursorMode.Auto);
+        }
+
+    }
+
+}
