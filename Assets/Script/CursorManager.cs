718b92f38f8fc579b780b714d2698e2ce72f6caa
wed2425 <54612425+wed2425@users.noreply.github.com>
2021-04-08 13:23:16 +0900

    Cursor(ver.2)
    커서 카메라 적용
    사진 데이터 저장
    leftCursorClicked 이미지 사이즈에 맞게 조절해야함

--- Assets/Script/CursorManager.cs
+++ Assets/Script/CursorManager.cs
@@ -7,28 +7,55 @@ public class CursorManager : MonoBehaviour
 
     public Texture2D cursor;
     public Texture2D leftCursorClicked;
-    public Vector2 MouseHotSpot = Vector2.zero;
+
+    public Vector2 cursorHotSpot = Vector2.zero;
     [SerializeField]
     public Vector2 leftClickedHotSpot;
+
+    Vector2 cursorPosition;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.SetCursor(cursor, MouseHotSpot, CursorMode.Auto);
-
+        Cursor.SetCursor(cursor, cursorHotSpot, CursorMode.Auto);
+       
     }
 
     private void Update()
     {
+
+       
+       
         if (Input.GetMouseButtonDown(0))
         {
+            cursorPosition = Input.mousePosition;
             Cursor.SetCursor(leftCursorClicked, leftClickedHotSpot, CursorMode.ForceSoftware);
+            StartCoroutine("Capture");
         }
         if(Input.GetMouseButtonUp(0))
         {
-            Cursor.SetCursor(cursor, MouseHotSpot, CursorMode.Auto);
+            Cursor.SetCursor(cursor, cursorHotSpot, CursorMode.Auto);
         }
 
     }
 
-}
+
+    IEnumerator Capture()
+    {
+        yield return new WaitForEndOfFrame();
+
+        byte[] imgBytes;
+        string filename = Application.productName + "_" + System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
+        string location = @"F:\Wish\Graduation-Project\Assets\ScreenShot\";
+        string finalLoc = location+filename;
+       // string path = @"F:\Wish\Graduation-Project\Assets\ScreenShot\test.png";
+        Texture2D screenShot = new Texture2D(leftCursorClicked.width, leftCursorClicked.height, TextureFormat.RGB24, false);
+        screenShot.ReadPixels(new Rect(cursorPosition.x -170, cursorPosition.y -124, leftCursorClicked.width , leftCursorClicked.height ), 0, 0, false);
+        screenShot.Apply();
+        imgBytes = screenShot.EncodeToPNG();
+        System.IO.File.WriteAllBytes(finalLoc, imgBytes);
+        Debug.Log(" has been saved");
+    }
+  
+};
