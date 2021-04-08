9b1b66fe4a0cf8faa160ad1322bb82772d888f9f
Yechani-0422 <63704943+Yechani-0422@users.noreply.github.com>
2021-04-08 17:14:35 +0900

    LoadSave(test2)

--- Assets/Script/Manager.cs
+++ Assets/Script/Manager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Pause : MonoBehaviour
+public class Manager : MonoBehaviour
 {
     public GameObject SubMenu;
     // Start is called before the first frame update
